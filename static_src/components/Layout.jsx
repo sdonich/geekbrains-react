3202160a993a21b00c2488775194195982f568c1
sdonich <sdonich@gmail.com>
2019-12-11 00:07:13 +0200

    Some fix

--- static_src/components/Layout.jsx
+++ static_src/components/Layout.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import MessageField from './MessageField';
 import { ChatList } from './ChatList';
 import { Header } from './Header';
+import '../styles/layout.css';
 
 export const Layout = () => {
   return (
     <div className="layout">
       <Header />
-      <ChatList />
-      <MessageField />
+      <div className="messages-content">
+        <div className="chatlist">
+          <ChatList />
+        </div>
+        <div className="chat">
+          <MessageField />
+        </div>
+      </div>
     </div>
   )
 }
