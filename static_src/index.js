efb3cc34e7d4d609a978c4010ae5ac2882bba7f4
Sergei <sdonich@gmail.com>
2019-12-24 16:31:14 +0200

    Install redux and other components

--- static_src/index.js
+++ static_src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import Router from './components/Router';
 import { BrowserRouter } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import store from './store';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<MuiThemeProvider>
-			<Router />
-		</MuiThemeProvider>
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<MuiThemeProvider>
+				<Router />
+			</MuiThemeProvider>
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root'),
 );
